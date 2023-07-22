abea9245df11fb740a463b49981f1b67373016b0
Coleman Matey <113218526+colemanmatey@users.noreply.github.com>
2023-07-22 09:38:02 +0000

    Create gulp task to copy and minify HTML

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@
 const { src, dest, watch } = require("gulp");
 const autoprefixer = require("gulp-autoprefixer");
 const cssnano = require("cssnano");
+const htmlmin = require("gulp-htmlmin");
 const postcss = require("gulp-postcss");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass")(require("sass"));
@@ -14,13 +15,16 @@ const sourcemaps = require("gulp-sourcemaps");
 // Paths
 let paths = {
 	source: {
+		html: "./src/**/*.html",
 		sass: "./src/assets/styles/sass/**/*.scss",
 		css: "./build/**/*.css"
 	},
 	build: {
+		html: "./build",
 		css: "./build/css",
 	},
 	dist: {
+		html: "./dist",
 		css: "./dist/css",
 	},
 };
@@ -48,6 +52,14 @@ function minifyCSS() {
 		.pipe(dest(paths.dist.css));
 }
 
+// Copy HTML
+function copyHTML() {
+	return src(paths.source.html)
+		.pipe(dest(paths.build.html))
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(dest(paths.dist.html))
+}
+
 // Monitor file changes
 function monitor() {
 	watch(paths.source.sass, transpileSass);
@@ -57,4 +69,5 @@ function monitor() {
 // Exports
 exports.transpileSass = transpileSass;
 exports.minifyCSS = minifyCSS;
+exports.copyHTML = copyHTML;
 exports.monitor = monitor;
